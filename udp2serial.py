6c1b0d0c2fd82180650ded20cd1a839e087ce708
Antoine Villeret <antoine.villeret@gmail.com>
2016-01-06 18:53:39 +0100

    this make a bridge between OSC program (Pd or Max) and a Teensy3.2

--- udp2serial.py
+++ udp2serial.py
@@ -1,11 +1,12 @@
 import socket,select,tty
 import slip
 import time
+import serial
 
 UDP_PORT_IN = 10000 # UDP server port
 UDP_PORT_OUT = 11000
-MAX_UDP_PACKET=512 # max size of incoming packet to avoid sending too much data to the micro
-SERIAL_PORT='/dev/ttyATH0'
+MAX_UDP_PACKET=4096 # max size of incoming packet to avoid sending too much data to the micro
+SERIAL_PORT='/dev/ttyACM0'
 BROADCAST_MODE=False #set to True if you want a broadcast replay instead of unicast
 
 
@@ -13,12 +14,16 @@ udp_client_address=('127.0.0.1',UDP_PORT_OUT) #where to forward packets coming f
 udp_server_address = ('',UDP_PORT_IN) #udp server
 udp_broadcast=('<broadcast>',UDP_PORT_OUT) #broadcast address
 
-serial=open(SERIAL_PORT,'r+b') #open the file corrisponding to YUN serial port
+#serial=open(SERIAL_PORT,'r+b') #open the file corrisponding to YUN serial port
+serial = serial.Serial(
+    port=SERIAL_PORT,
+    baudrate=921600)
+
 tty.setraw(serial) #this avoids the terminal to change bytes with value 10 in 13 10
 
 udp_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 udp_socket.bind(udp_server_address)
-if BROADCAST_MODE: 
+if BROADCAST_MODE:
         udp_socket.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
 
 slip_processor=slip.slip()
@@ -33,8 +38,8 @@ while True:
                         if BROADCAST_MODE:
                                 udp_socket.sendto(packet,udp_broadcast)
                         else:
-                                udp_socket.sendto(packet,udp_client_address)        
-                                
+                                udp_socket.sendto(packet,udp_client_address)
+
         if udp_socket in rlist:
                 udp_data,udp_client = udp_socket.recvfrom(MAX_UDP_PACKET)
                 slip_data=slip_processor.encode(udp_data)
